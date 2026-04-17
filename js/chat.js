48258d68085ea500cf7b14b2355d6169ccd347e7
WhtTheHel <karmanuserbot@gmail.com>
2026-04-18 00:20:53 +0700

    Refactor chat message handling in chat.js

--- js/chat.js
+++ js/chat.js
@@ -1,22 +1,28 @@
 import { db } from "./firebase.js";
-import {
-  addDoc, collection, onSnapshot, orderBy, query
-} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+import { addDoc, collection, onSnapshot, orderBy, query } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
 
-window.sendChat = async () => {
-  await addDoc(collection(db, "chats"), {
-    msg: chatMsg.value,
-    time: Date.now()
+let lastCount=0;
+
+window.sendChat = async ()=>{
+  await addDoc(collection(db,"chats"),{
+    msg:chatMsg.value,
+    time:Date.now()
   });
 
-  chatMsg.value = "";
+  chatMsg.value="";
 };
 
-onSnapshot(query(collection(db, "chats"), orderBy("time")), snap => {
-  chatBox.innerHTML = "";
+onSnapshot(query(collection(db,"chats"),orderBy("time")),snap=>{
+  chatBox.innerHTML="";
 
-  snap.forEach(d => {
-    const c = d.data();
-    chatBox.innerHTML += `<div class="card">${c.msg}</div>`;
+  if(snap.size>lastCount){
+    notifSound.play();
+  }
+
+  lastCount = snap.size;
+
+  snap.forEach(d=>{
+    const c=d.data();
+    chatBox.innerHTML+=`<div class="card">${c.msg}</div>`;
   });
 });
