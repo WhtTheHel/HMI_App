c18adf751f5275f2eb598ee2b9f3a1dca297ee7e
WhtTheHel <karmanuserbot@gmail.com>
2026-04-18 00:20:10 +0700

    Refactor feed.js for cleaner syntax and formatting

--- js/feed.js
+++ js/feed.js
@@ -1,40 +1,34 @@
 import { db, storage } from "./firebase.js";
-import {
-  addDoc, collection, onSnapshot, orderBy, query
-} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+import { addDoc, collection, onSnapshot, orderBy, query } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+import { ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-storage.js";
 
-import {
-  ref, uploadBytes, getDownloadURL
-} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-storage.js";
-
-btnPost.onclick = async () => {
+btnPost.onclick = async ()=>{
   const file = pFile.files[0];
-  let url = "";
+  let url="";
 
-  if (file) {
-    const r = ref(storage, "posts/" + Date.now());
-    await uploadBytes(r, file);
+  if(file){
+    const r = ref(storage,"posts/"+Date.now());
+    await uploadBytes(r,file);
     url = await getDownloadURL(r);
   }
 
-  await addDoc(collection(db, "posts"), {
-    text: pInput.value,
-    media: url,
-    time: Date.now()
+  await addDoc(collection(db,"posts"),{
+    text:pInput.value,
+    media:url,
+    time:Date.now()
   });
 
-  pInput.value = "";
+  showToast("Posting berhasil");
 };
 
-onSnapshot(query(collection(db, "posts"), orderBy("time", "desc")), snap => {
-  feed.innerHTML = "";
-  snap.forEach(d => {
-    const p = d.data();
-    feed.innerHTML += `
-      <div class="card">
-        ${p.text}
-        ${p.media ? `<img src="${p.media}" width="100%">` : ""}
-      </div>
-    `;
+onSnapshot(query(collection(db,"posts"),orderBy("time","desc")),snap=>{
+  feed.innerHTML="";
+  snap.forEach(d=>{
+    const p=d.data();
+    feed.innerHTML+=`
+    <div class="card">
+      ${p.text}
+      ${p.media?`<img src="${p.media}" width="100%">`:""}
+    </div>`;
   });
 });
